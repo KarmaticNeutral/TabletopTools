7b670a262daf6fcccc60a598ccebc2641d231491
Angelo Pablo <angelopablo014@gmail.com>
2019-02-20 13:31:18 -0700

    TEEHEE

--- app/src/main/java/com/example/Table_Top_Gaming/Game.java
+++ app/src/main/java/com/example/Table_Top_Gaming/Game.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
 
+    //Blah
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

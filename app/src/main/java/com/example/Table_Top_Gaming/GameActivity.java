852f8c553b5a0b4091e16a1fb086dad38e954f7b
SonoftheRightHand <tha17001@byui.edu>
2019-02-21 16:44:16 -0700

    These changes allow navigation from window to window.

---
+++ app/src/main/java/com/example/Table_Top_Gaming/GameActivity.java
@@ -0,0 +1,42 @@
+package com.example.Table_Top_Gaming;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+        // Get the intent for this Activity
+        Intent intent = getIntent();
+
+        // Grab the information passed to this Activity and place it in the string "message"
+        String message = intent.getStringExtra(LoadGameActivity.EXTRA_MESSAGE);
+
+        // Replace the Text on this Activity with the information of "message" and display it to the
+        // screen
+        TextView textView = findViewById(R.id.textView2);
+
+        if (message == null) {
+            message = intent.getStringExtra(SaveGameActivity.EXTRA_MESSAGE_SAVE);
+
+            if (message == null) {
+                message = "New Game";
+            }
+        }
+        textView.setText(message);
+
+    }
+
+    public void saveGame(View view) {
+        Intent intent = new Intent(this, SaveGameActivity.class);
+        startActivity(intent);
+    }
+}

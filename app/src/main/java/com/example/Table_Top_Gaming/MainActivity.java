b5dd17fd4e6aadb954a0c6c5d854236d28540073
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-03-06 13:53:16 -0700

    Added basic SharedPreferences for login info.

--- app/src/main/java/com/example/Table_Top_Gaming/MainActivity.java
+++ app/src/main/java/com/example/Table_Top_Gaming/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.Table_Top_Gaming;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +13,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getPreferences(Context.MODE_PRIVATE);
     }
 
     /*
@@ -31,9 +36,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
     public void onTakePhoto(View view) {
         Intent intent = new Intent (MainActivity.this, CameraActivity.class);
         startActivity(intent);
+    }
+
+    public void signIn(View view){
+        String cloudUsername = prefs.getString("cloudUsername", " ");
+        String cloudPassword = prefs.getString("cloudPassword", " ");
+
+        if (cloudUsername.equals(" ")) {
+            //go to get info activity
+        }
+        else {
+            // Connect to FireBase
+        }
     }
 }

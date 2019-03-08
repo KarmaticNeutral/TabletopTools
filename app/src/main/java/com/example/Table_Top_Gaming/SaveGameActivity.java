45fbf2f9f43e9ee8c0af498af3e2685e9e9fe978
Angelo Pablo <angelopablo014@gmail.com>
2019-03-07 20:05:26 -0700

    Cloud save and load yayyyy!!!!

--- app/src/main/java/com/example/Table_Top_Gaming/SaveGameActivity.java
+++ app/src/main/java/com/example/Table_Top_Gaming/SaveGameActivity.java
@@ -1,19 +1,31 @@
 package com.example.Table_Top_Gaming;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SaveGameActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE_SAVE = "com.example.Table_Top_Gaming.MESSAGE2";
+    public static final String TAG = "hey";
     private String fileName;
     private String message;
     private Intent intent;
@@ -30,60 +42,67 @@ public class SaveGameActivity extends AppCompatActivity {
         message = intent.getExtras().getString("Game");
 
         // Create a TextView object and set the text field to the information stored in message
-        TextView textView = findViewById(R.id.textView7);
-        textView.setText(message);
+//        TextView textView = findViewById(R.id.saveNameTextBox);
+//        textView.setText(message);
 
-    }
-
-    public void saveOne(View view) {
-        fileName = "save.txt";
-
-        createSave();
-    }
-
-    public void saveTwo(View view) {
-        fileName = "save2.txt";
-
-        createSave();
-    }
-
-    public void saveThree(View view) {
-        fileName = "save3.txt";
-
-        createSave();
     }
 
     /*
         This function creates a save file and writes the game information to it
     */
-    public void createSave() {
-        // Create the object that it can open files
-        FileOutputStream fos = null;
+    public void createSave(View view) {
 
-        // Open "save.txt" and write the message in "temp" in it
-        // Next display a toast showing the file path and files saved to
-        try {
-            fos = openFileOutput(fileName, MODE_PRIVATE);
-            fos.write(message.getBytes());
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-            // Display the file path the information is being saved to
-            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + fileName,
-                    Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        Map <String, Object> saveGame = new HashMap<>();
+        TextView textView = findViewById(R.id.saveNameTextBox);
+        String saveName = textView.getText().toString();
 
-            // Close the file
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        saveGame.put("Name", saveName);
+        saveGame.put("savedGame", message);
+        db.collection("savedGames")
+                .add(saveGame)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+
+//        // Create the object that it can open files
+//        FileOutputStream fos = null;
+//
+//        // Open "save.txt" and write the message in "temp" in it
+//        // Next display a toast showing the file path and files saved to
+//        try {
+//            fos = openFileOutput(fileName, MODE_PRIVATE);
+//            fos.write(message.getBytes());
+//
+//            // Display the file path the information is being saved to
+//            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + fileName,
+//                    Toast.LENGTH_LONG).show();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//
+//            // Close the file
+//        } finally {
+//            if (fos != null) {
+//                try {
+//                    fos.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
 
         // After the information is saved pass back the game information and restart the
         // GameActivity

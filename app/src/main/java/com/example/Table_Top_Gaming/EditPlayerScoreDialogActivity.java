a1f91bb8a02743d5f9fc45ae2f396159c2fa463d
SonoftheRightHand <tha17001@byui.edu>
2019-03-14 00:42:45 -0600

    Made the player score button that can update the player score

---
+++ app/src/main/java/com/example/Table_Top_Gaming/EditPlayerScoreDialogActivity.java
@@ -0,0 +1,13 @@
+package com.example.Table_Top_Gaming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class EditPlayerScoreDialogActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_player_score_dialog);
+    }
+}
--- app/src/main/java/com/example/Table_Top_Gaming/GameActivity.java
+++ app/src/main/java/com/example/Table_Top_Gaming/GameActivity.java
@@ -1,9 +1,13 @@
 package com.example.Table_Top_Gaming;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +16,7 @@ import com.google.gson.Gson;
 public class GameActivity extends AppCompatActivity {
     private Game game;
     private Gson gson = new Gson();
+    private Button scoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,12 @@ public class GameActivity extends AppCompatActivity {
         // Parse the information saved in the String "message" and place it in the Game object
         game = gson.fromJson(message, Game.class);
 
+        // Testing Player Score button
+        game.getPlayers().get(0).setScore(10);
+        scoreButton = findViewById(R.id.player_score_button);
+        // Set the Text on the button equal to the first players score
+        scoreButton.setText(Integer.toString(game.getPlayers().get(0).getScore()));
+
     }
 
     /*
@@ -67,7 +78,6 @@ public class GameActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         }
         String message = gson.toJson(game);
-        System.out.println(message);
     }
 
     public void drawHand(View view) {
@@ -82,6 +92,90 @@ public class GameActivity extends AppCompatActivity {
         game.getPlayers().get(player).setHand(game.getDeck().drawHand(numCards));
 
         String message = gson.toJson(game);
-        System.out.println(message);
+    }
+
+    /*
+    This function Allows the editing of a players score when the score button is pressed
+     */
+    public void editPlayerScore (View view2) {
+        // Define a new AlertDialog box that will be called from this activity
+        AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+
+        // Using a custom Layout from the EditPlayerScoreDialogActivity so we have to get that view
+        View view = getLayoutInflater().inflate(R.layout.activity_edit_player_score_dialog, null);
+
+        // Get the text field where the user will input a change in score
+        final EditText input = (EditText) view.findViewById(R.id.difference_in_score);
+
+        // Get the text view that displays the players score on the new activity window
+        final TextView difference = (TextView) view.findViewById(R.id.player_score);
+
+        // Set the score display on the new window equal to Player 1's score
+        difference.setText(Integer.toString(game.getPlayers().get(0).getScore()));
+
+        // There is a plus and a minus button on this custom layout so we have to grab them
+        // and define them in this activity
+        Button plus = (Button) view.findViewById(R.id.plus_button);
+        Button minus = (Button) view.findViewById(R.id.minus_button);
+
+        // When the user clicks the plus button add the input to the player's score
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set this integer to the amount the user wants to change the score by
+                int diff = Integer.parseInt(input.getText().toString());
+
+                // Add this amount to the current player score
+                game.getPlayers().get(0).setScore(game.getPlayers().get(0).getScore() + diff);
+
+                // Change the display on the new window to the new amount
+                difference.setText(Integer.toString(game.getPlayers().get(0).getScore()));
+
+                // Change the display on the GameActivity window score button to the new amount
+                scoreButton.setText(Integer.toString(game.getPlayers().get(0).getScore()));
+            }
+        });
+
+        // When the user clicks the minus button subtract the input from the player's score
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set this integer to the amount the user wants to change the score by
+                int diff = Integer.parseInt(input.getText().toString());
+
+                // Subtract this amount from the current player score
+                game.getPlayers().get(0).setScore(game.getPlayers().get(0).getScore() - diff);
+
+                // Change the display on the new window to the new amount
+                difference.setText(Integer.toString(game.getPlayers().get(0).getScore()));
+
+                // Change the display on the GameActivity window score button to the new amount
+                scoreButton.setText(Integer.toString(game.getPlayers().get(0).getScore()));
+            }
+        });
+
+        builder.setView(view)
+                .setPositiveButton(R.string.player_score_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The user hit "OK" do nothing they are done
+                    }
+                }).setNeutralButton(R.string.change_score_to, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // Change the score to the number the user input instead of adding or subtracting it
+                        game.getPlayers().get(0).setScore(Integer.parseInt(input.getText().toString()));
+                        scoreButton.setText(Integer.toString(game.getPlayers().get(0).getScore()));
+                    }
+                })
+                .setNegativeButton(R.string.player_score_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The user hit "CANCEL" do nothing they are done
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }

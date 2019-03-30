532c2a580d73154452787cad5787c59e9ba2a749
karmaticneutral <karmaticneutral@gmail.com>
2019-03-30 12:06:52 -0600

    Basic Grid View in place. A lot to still do, but it doesn't crash the game and you can see the scores/players grid.

--- app/src/main/java/com/example/Table_Top_Gaming/GridViewActivity.java
+++ app/src/main/java/com/example/Table_Top_Gaming/GridViewActivity.java
@@ -1,9 +1,18 @@
 package com.example.Table_Top_Gaming;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -32,5 +41,47 @@ public class GridViewActivity extends AppCompatActivity {
         game = gson.fromJson(message, Game.class);
 
         numPlayers = game.getPlayers().size();
+
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.table_main);
+
+
+        //Start at -1 so that You get a row showing player names first
+        for (int i = -1; i < game.getPlayers().get(0).getResources().size(); i++) {
+            TableRow row = new TableRow(this);
+            //Start at -1 so that you get a collum of resource names first
+            for (int j = -1; j < game.getPlayers().size(); j++) {
+                Log.d(TAG, "onCreate: I: " + i + ", J: " + j);
+                if (i == -1) {
+                    TextView textView = new TextView(this);
+                    textView.setWidth(100);
+                    textView.setHeight(50);
+                    textView.setTextColor(Color.WHITE);
+                    if (j == -1) {
+                        textView.setText("");
+                    } else {
+                        textView.setText(game.getPlayers().get(j).getName());
+                    }
+                    row.addView(textView);
+                } else if (j == -1) {
+                    TextView textView = new TextView(this);
+                    textView.setWidth(100);
+                    textView.setHeight(50);
+                    textView.setText(game.getPlayers().get(0).getResources().get(i).getName());
+                    textView.setTextColor(Color.WHITE);
+                    row.addView(textView);
+                } else {
+                    if (game.getPlayers().get(j).getResources() != null) {
+                        Button button = new Button(this);
+                        button.setWidth(100);
+                        button.setHeight(50);
+                        button.setText("" + game.getPlayers().get(j).getResources().get(i).getAmount());
+                        row.addView(button);
+                    }
+                }
+                if(j == game.getPlayers().size() - 1) {
+                    tableLayout.addView(row, i);
+                }
+            }
+        }
     }
 }

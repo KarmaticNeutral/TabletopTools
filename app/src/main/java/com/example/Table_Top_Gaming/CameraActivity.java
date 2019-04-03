2b8dd5927ecc0fdeb2c90034bb36ffeb8d510b63
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-04-03 14:07:58 -0600

    Added camera & gallery permissions requests.

--- app/src/main/java/com/example/Table_Top_Gaming/CameraActivity.java
+++ app/src/main/java/com/example/Table_Top_Gaming/CameraActivity.java
@@ -1,7 +1,9 @@
 package com.example.Table_Top_Gaming;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.icu.text.SimpleDateFormat;
@@ -10,6 +12,8 @@ import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,8 +28,11 @@ import java.io.IOException;
 import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 3;
+    private static final int MY_PERMISSIONS_REQUEST_READ_STORAGE = 4;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_STORAGE = 5;
     ImageView imageView;
-    public static final String TAG = "CameraActivity";
+    private static final String TAG = "CameraActivity";
     private final int GALLERY_REQUEST_CODE = 1;
     private final int CAMERA_REQUEST_CODE = 2;
     private Gson gson = new Gson();
@@ -47,48 +54,72 @@ public class CameraActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Opens the default camera app and lets the user take a picture.
+     * @param view Camera button
+     */
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void captureFromCamera(View view) {
-        /*
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-        }
-        */
 
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                Log.e(TAG, "Error creating file.");
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+        // Make sure that the user has given camera permissions.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission denied");
+
+            // If permission is denied, ask for permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+        else {
+            // Ensure that there's a camera activity to handle the intent
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                // Create the File where the photo should go
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    // Error occurred while creating the File
+                    Log.e(TAG, "Error creating file.");
+                }
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "com.example.android.fileprovider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+                }
             }
         }
     }
 
-    // Opens default photo gallery app, and allows user to choose image.
+    /**
+     * Opens the default gallery app and lets the user choose an image.
+     * @param view Gallery button.
+     */
     public void pickFromGallery(View view) {
-        //Create an Intent with action as ACTION_PICK
+        //Create an Intent with action as ACTION_OPEN_DOCUMENT
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        // Sets the type as image/*. This ensures only components of type image are selected
-        intent.setType("image/*");
-        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
-        String[] mimeTypes = {"image/jpeg", "image/png"};
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-        // Launching the Intent
-        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+
+        // Make sure that the user has given storage permissions.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Write storage permission denied");
+
+            // If permission is denied, ask for permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+        }
+        else {
+            // Sets the type as image/*. This ensures only components of type image are selected
+            intent.setType("image/*");
+            //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+            String[] mimeTypes = {"image/jpeg", "image/png"};
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+            // Launching the Intent
+            startActivityForResult(intent, GALLERY_REQUEST_CODE);
+        }
     }
 
     private String cameraFilePath;
@@ -115,6 +146,12 @@ public class CameraActivity extends AppCompatActivity {
         return image;
     }
 
+    /**
+     * Sets the image view to the selected picture, and stores that picture as a URI
+     * @param requestCode Checks if the user has selected an image.
+     * @param resultCode Checks if the image was selected from camera or gallery
+     * @param data The data which will store the image URI which will be returned to game activity.
+     */
     public void onActivityResult(int requestCode,int resultCode,Intent data){
         // Result code is RESULT_OK only if the user selects an Image
         if (resultCode == Activity.RESULT_OK)
@@ -135,6 +172,10 @@ public class CameraActivity extends AppCompatActivity {
             }
     }
 
+    /**
+     * Takes the user back to the game activity.
+     * @param view The screen which contains a button that returns to the Game Activity.
+     */
     public void returnToGame(View view) {
         String gameGson = gson.toJson(game);
         Intent intent = new Intent(this, GameActivity.class);
